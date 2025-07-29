8146f01edf75b93b03ac62dce7991f0e46e717b5
Tim Goodwin <timg.goodwin@gmail.com>
2025-07-29 09:48:44 -0500

    update run all script to my liking

--- run_all.sh
+++ run_all.sh
@@ -2,6 +2,7 @@
 
 MANIFEST_RUN='full'
 WORKLOAD_TYPE='save'
+RESULTS_DIR='results-32cores'
 
 init_social_graph() {
     python demo/monolith/test/init_social_graph.py --ip $(get_public_ip)  --port 80 --compose
@@ -48,10 +49,11 @@ run_step() {
     init_social_graph
     # wait 10 seconds before running the load test
     echo "Waiting for 10 seconds before running the load test..."
+    sleep 10
     echo "Running load test for $1..."
     # Run the load test
     # python throughput_test.py --ip $(get_public_ip) --port 80 --output results/$1-throughput-01.csv
-    run_throughput results-$WORKLOAD_TYPE/$1-throughput-$2.csv 
+    run_throughput $RESULTS_DIR/$1-throughput-01.csv 
 
     echo "Load test for $1 completed."
     echo "Cleaning up resources for $1..."
@@ -62,24 +64,24 @@ run_step() {
 
 run_all() {
     echo "Running all steps..."
+    mkdir -p $RESULTS_DIR
+
     deploy_shared_manifests
 
     WORKLOAD_TYPE='save'
     echo "Running mixed workload..."
-    for i in {1..3}; do
+    # for i in {1..3}; do
         echo "Iteration $i..."
-        run_step "full" "$i"
-        run_step "monolith" "$i"
-        # run_step "monolith_large" "$i"
-#        run_step "monolith_small" "$i"
-        run_step "user" "$i"
-        run_step "socialgraph" "$i"
-        run_step "timeline" "$i"
-        run_step "post" "$i"
+    run_step "monolith" "$i"
+    run_step "full" "$i"
+    run_step "user" "$i"
+    run_step "socialgraph" "$i"
+    run_step "timeline" "$i"
+    run_step "post" "$i"
         # run_step "mixed_profile_half_peak" "$i"
         # run_step "save_profile_half_peak" "$i"
         # run_step "save_profile_peak" "$i"
-        done
+        # done
     echo "All steps completed."
 
     # WORKLOAD_TYPE='mixed'
