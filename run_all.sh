d89209ee6ba5b9947584d6401ed0f3b8a821c232
Tim Goodwin <timg.goodwin@gmail.com>
2025-07-28 22:04:23 -0500

    fuckl it

--- run_all.sh
+++ run_all.sh
@@ -8,21 +8,21 @@ init_social_graph() {
 }
 
 run_throughput() {
-    if [[ $WORKLOAD_TYPE == "save" ]]; then
-        go run loadgen.go --ip   $(get_public_ip) --port 80 --output-file $1 --early-exit --workload save
-    else 
-        go run loadgen.go --ip   $(get_public_ip) --port 80 --output-file $1 --early-exit --workload mixed
-    fi
+    # if [[ $WORKLOAD_TYPE == "save" ]]; then
+    go run loadgen.go --ip   $(get_public_ip) --port 80 --output-file $1 --early-exit --workload save
+    # else 
+    #     go run loadgen.go --ip   $(get_public_ip) --port 80 --output-file $1 --early-exit --workload mixed
+    # fi
 }
 
 get_public_ip() {
     # Get the public IP address of the Kubernetes cluster
-    if [[ $MANIFEST_RUN == "monolith" ]]; then
+    if [ $MANIFEST_RUN == "monolith" ]; then
         kubectl get svc monolith-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
-    elif [[ $MANIFEST_RUN == "monolith_large" ]]; then
-        kubectl get svc monolith-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
-    elif [[ $MANIFEST_RUN == "monolith_small" ]]; then
-        kubectl get svc monolith-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
+    # elif [[ $MANIFEST_RUN == "monolith_large" ]]; then
+    #     kubectl get svc monolith-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
+    # elif [[ $MANIFEST_RUN == "monolith_small" ]]; then
+    #     kubectl get svc monolith-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
     else 
         kubectl get svc entrypoint-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
     fi
@@ -35,7 +35,6 @@ deploy_shared_manifests() {
     echo "Shared manifests deployed."
 }   
 
-
 run_step() {
     MANIFEST_RUN=$1
     echo "Running step: $1"
@@ -44,7 +43,7 @@ run_step() {
     sleep 10
     echo "Applying manifests for $1..."
     kubectl apply -f manifests/$1
-    sleep 20
+    sleep 10
     echo "initializing social graph..."
     init_social_graph
     # wait 10 seconds before running the load test
@@ -52,7 +51,7 @@ run_step() {
     echo "Running load test for $1..."
     # Run the load test
     # python throughput_test.py --ip $(get_public_ip) --port 80 --output results/$1-throughput-01.csv
-    run_throughput results-$WORKLOAD_TYPE/$1/throughput-$2.csv 
+    run_throughput results-$WORKLOAD_TYPE/$1-throughput-$2.csv 
 
     echo "Load test for $1 completed."
     echo "Cleaning up resources for $1..."
@@ -71,35 +70,35 @@ run_all() {
         echo "Iteration $i..."
         run_step "full" "$i"
         run_step "monolith" "$i"
-        run_step "monolith_large" "$i"
-        run_step "monolith_small" "$i"
+        # run_step "monolith_large" "$i"
+#        run_step "monolith_small" "$i"
         run_step "user" "$i"
         run_step "socialgraph" "$i"
         run_step "timeline" "$i"
         run_step "post" "$i"
-        run_step "mixed_profile_half_peak" "$i"
-        run_step "save_profile_half_peak" "$i"
-        run_step "save_profile_peak" "$i"
+        # run_step "mixed_profile_half_peak" "$i"
+        # run_step "save_profile_half_peak" "$i"
+        # run_step "save_profile_peak" "$i"
         done
     echo "All steps completed."
 
-    WORKLOAD_TYPE='mixed'
-    echo "Running mixed workload..."
+    # WORKLOAD_TYPE='mixed'
+    # echo "Running mixed workload..."
 
-    for i in {1..3}; do
-        echo "Iteration $i..."
-        run_step "full" "$i"
-        run_step "monolith" "$i"
-        run_step "monolith_large" "$i"
-        run_step "monolith_small" "$i"
-        run_step "user" "$i"
-        run_step "socialgraph" "$i"
-        run_step "timeline" "$i"
-        run_step "post" "$i"
-        run_step "mixed_profile_half_peak" "$i"
-        run_step "save_profile_half_peak" "$i"
-        run_step "save_profile_peak" "$i"
-    done
+    # for i in {1..3}; do
+    #     echo "Iteration $i..."
+    #     run_step "full" "$i"
+    #     run_step "monolith" "$i"
+    #     run_step "monolith_large" "$i"
+    #     run_step "monolith_small" "$i"
+    #     run_step "user" "$i"
+    #     run_step "socialgraph" "$i"
+    #     run_step "timeline" "$i"
+    #     run_step "post" "$i"
+    #     run_step "mixed_profile_half_peak" "$i"
+    #     run_step "save_profile_half_peak" "$i"
+    #     run_step "save_profile_peak" "$i"
+    # done
 }
 
 run_all
