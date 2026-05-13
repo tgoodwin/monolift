c9afa6172d2b5e1e5ae1f1f85cef913606e5ee2a
Tim Goodwin <timg.goodwin@gmail.com>
2026-05-13 11:48:18 -0700

    add c6220

--- profile.py
+++ profile.py
@@ -15,7 +15,8 @@ pc.defineParameter("hardware_type", "Hardware type",
                     ("c6525-100g", "c6525-100g (24-core AMD 7402P, 128GB, Utah)"),
                     ("xl170", "xl170 (10-core Xeon E5-2640v4, 64GB, Utah)"),
                     ("c220g5", "c220g5 (20-core Xeon Silver 4114, 192GB, Wisconsin)"),
-                    ("c6420", "c6420 (32-core Xeon Gold 6142, 384GB, Clemson)")],
+                    ("c6420", "c6420 (32-core Xeon Gold 6142, 384GB, Clemson)"),
+                    ("c6220", "c6220 (16-core Xeon E5-2650v2, 64GB, Apt)")],
                    longDescription="Pick based on availability. c6525-25g is a good default: "
                    "16 cores, 128GB RAM, dual 480GB SSDs.")
 
